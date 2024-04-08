214daff380490585fce32762e0b94fbb936352e9
vundi4773 <vundi4773@gmail.com>
2024-04-08 23:30:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+let employee = {
+    name: " Kinyajui",
+    streetAddress: " 48 Riverside lane"
+};
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    let updateEmployee = { ...employee};
+    updateEmployee [ key] = value;
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue( employee, key, value) {
+   employee[key] = value;
+   return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value)  {
+     let deleteFromEmployee = {...employee}
+    delete deleteFromEmployee[key];
+  
+  return deleteFromEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+  }
